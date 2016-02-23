513e1cfb4511f3136ca7d3040f52f3c7bcaf26ec
claudia@claudiag.me.uk <claudia@claudiag.me.uk>
2016-02-23 16:12:11 +0000

    first commit

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+  'fizz'
+end
+#
+# num.each do |x|
+#       if x % 15 == 0
+#         puts "fizz buzz"
+#       elsif x % 3 == 0
+#         puts "fizz"
+#       elsif x % 5 == 0
+#         puts "buzz"
+#       else
+#       puts x
+#       end
+# end
